d9149cd94abf057dae8d28cbb465b484f291a110
PopaGeorge123 <popageo02@gmail.com>
2024-05-04 17:13:17 +0300

    ready to launch

--- app/del/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { saveFile , cleanFolder } from "@/local/local";
-
-export async function GET(request) {
-    //"chart.png"
-    await cleanFolder("down.svg")
-    
-    return new NextResponse(null, { status: 200 });
-}
